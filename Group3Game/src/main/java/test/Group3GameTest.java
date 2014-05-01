f9e8ec62d9d400ea1e1e33d0e8f7a46b4d32ffc4
BWONG <BWONG@BWONG-PC>
2014-04-30 19:34:51 -0500

    More subtests

--- Group3Game/src/main/java/test/Group3GameTest.java
+++ Group3Game/src/main/java/test/Group3GameTest.java
@@ -1,34 +1,12 @@
 package test;
 
-
+import org.junit.Before;
 import org.junit.Test;
-import android.content.pm.ActivityInfo;
-import android.util.Log;
-
 
 import junit.framework.TestCase;
-import android.test.InstrumentationTestRunner;
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.ContextMenu;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnFocusChangeListener;
-import android.widget.EditText;
 
-import edu.luc.etl.cs313.android.group3Game.R;
-import edu.luc.etl.cs313.android.group3Game.model.AutoRandomEngine;
-import edu.luc.etl.cs313.android.group3Game.model.Dot;
-import edu.luc.etl.cs313.android.group3Game.model.Dots;
 import edu.luc.etl.cs313.android.group3Game.view.DotView;
 import edu.luc.etl.cs313.android.group3Game.TouchMe;
-import edu.luc.etl.cs313.android.group3Game.TouchMe.*;
 
 
 import static org.junit.Assert.assertEquals;
@@ -49,9 +27,15 @@ public class Group3GameTest extends TestCase {
     public void checkInitialSettings() {
         assertEquals(0, TouchMe.MONSTERS);
         assertEquals(1, TouchMe.LEVEL);
-        //EditText et1 = (EditText) new TouchMe().findViewById(R.id.text1);
-        //EditText et2 = (EditText) new TouchMe().findViewById(R.id.text2);
-        //TouchMe().dotGenerator
-        //assertEquals(0, et1.getText());
+        TouchMe tm = new TouchMe();
+        DotView dv;
+        tm.runOnUiThread(new Runnable() {
+            @Override
+           public void run() {
+                assertEquals(2, TouchMe.MONSTERS);
+                assertEquals(1, TouchMe.LEVEL);
+
+            }
+        });
     }
 }
